c7c0ce5549fc890e03431ac14d1d0f24a7c849c5
Claudiu Nechitescu <cnechitescu@ahora.es>
2022-04-26 00:45:09 +0200

    Added pages and nodes linked to main objects

--- ZulemaBD/scripts/data/navigation_nodes.sql
+++ ZulemaBD/scripts/data/navigation_nodes.sql
@@ -1,1 +1,91 @@
 ﻿
+
+BEGIN TRY
+
+MERGE INTO [Navigation_Nodes] AS Target
+USING (VALUES
+  (N'1BF27F47-4D13-4F55-9ADF-0C9D4FD232BE',N'25A4B4D5-44AE-4CF1-8733-568F00F31520',1,N'Calendario',N'vcalendar-1',N'Calendario',N'page',NULL,NULL,N'current',NULL,NULL,N'z_pg_calendario_paciente',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,NULL,1)
+ ,(N'ECC37505-957A-4FEB-839B-1BDA01971A12',N'25A4B4D5-44AE-4CF1-8733-568F00F31520',2,N'Informes',N'file-text-o',N'Informes',N'page',NULL,NULL,N'current',NULL,NULL,N'z_pg_informes_paciente',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,NULL,1)
+ ,(N'64BD8171-B0A8-4ECC-90CD-5663580B1D0C',N'25A4B4D5-44AE-4CF1-8733-568F00F31520',3,N'Especialistas',N'follow-3',N'Especialistas',N'page',NULL,NULL,N'current',NULL,NULL,N'z_pg_especialistas',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,NULL,1)
+ ,(N'C3AC24E9-B555-4AD3-BA9D-890701BFA8F7',N'25A4B4D5-44AE-4CF1-8733-568F00F31520',5,N'Progreso',N'combine-chart',N'Progreso',N'page',NULL,NULL,N'current',NULL,NULL,N'z_pg_progreso_paciente',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,NULL,1)
+ ,(N'EC859B38-4263-4327-A38E-DB9C377D346C',N'25A4B4D5-44AE-4CF1-8733-568F00F31520',4,N'Chat',N'chat-online-1',N'Chat',N'page',NULL,NULL,N'current',NULL,NULL,N'z_pg_chat',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,1,NULL,1)
+) AS Source ([NodeId],[ParentNodeId],[Order],[Title],[IconName],[Descrip],[TypeId],[Params],[Url],[TargetId],[ProcessName],[PageTypeId],[PageName],[ReportName],[HelpId],[ReportWhere],[ObjectName],[ObjectWhere],[Defaults],[SQLSentence],[SQLConStringId],[WebComponent],[TableName],[BadgeClass],[BadgeSQL],[BadgeConStringId],[BadgeRefresh],[Enabled],[cssClass],[OriginId])
+ON (Target.[NodeId] = Source.[NodeId])
+WHEN MATCHED AND (
+	NULLIF(Source.[ParentNodeId], Target.[ParentNodeId]) IS NOT NULL OR NULLIF(Target.[ParentNodeId], Source.[ParentNodeId]) IS NOT NULL OR 
+	NULLIF(Source.[Order], Target.[Order]) IS NOT NULL OR NULLIF(Target.[Order], Source.[Order]) IS NOT NULL OR 
+	NULLIF(Source.[Title], Target.[Title]) IS NOT NULL OR NULLIF(Target.[Title], Source.[Title]) IS NOT NULL OR 
+	NULLIF(Source.[IconName], Target.[IconName]) IS NOT NULL OR NULLIF(Target.[IconName], Source.[IconName]) IS NOT NULL OR 
+	NULLIF(Source.[Descrip], Target.[Descrip]) IS NOT NULL OR NULLIF(Target.[Descrip], Source.[Descrip]) IS NOT NULL OR 
+	NULLIF(Source.[TypeId], Target.[TypeId]) IS NOT NULL OR NULLIF(Target.[TypeId], Source.[TypeId]) IS NOT NULL OR 
+	NULLIF(Source.[Params], Target.[Params]) IS NOT NULL OR NULLIF(Target.[Params], Source.[Params]) IS NOT NULL OR 
+	NULLIF(Source.[Url], Target.[Url]) IS NOT NULL OR NULLIF(Target.[Url], Source.[Url]) IS NOT NULL OR 
+	NULLIF(Source.[TargetId], Target.[TargetId]) IS NOT NULL OR NULLIF(Target.[TargetId], Source.[TargetId]) IS NOT NULL OR 
+	NULLIF(Source.[ProcessName], Target.[ProcessName]) IS NOT NULL OR NULLIF(Target.[ProcessName], Source.[ProcessName]) IS NOT NULL OR 
+	NULLIF(Source.[PageTypeId], Target.[PageTypeId]) IS NOT NULL OR NULLIF(Target.[PageTypeId], Source.[PageTypeId]) IS NOT NULL OR 
+	NULLIF(Source.[PageName], Target.[PageName]) IS NOT NULL OR NULLIF(Target.[PageName], Source.[PageName]) IS NOT NULL OR 
+	NULLIF(Source.[ReportName], Target.[ReportName]) IS NOT NULL OR NULLIF(Target.[ReportName], Source.[ReportName]) IS NOT NULL OR 
+	NULLIF(Source.[HelpId], Target.[HelpId]) IS NOT NULL OR NULLIF(Target.[HelpId], Source.[HelpId]) IS NOT NULL OR 
+	NULLIF(Source.[ReportWhere], Target.[ReportWhere]) IS NOT NULL OR NULLIF(Target.[ReportWhere], Source.[ReportWhere]) IS NOT NULL OR 
+	NULLIF(Source.[ObjectName], Target.[ObjectName]) IS NOT NULL OR NULLIF(Target.[ObjectName], Source.[ObjectName]) IS NOT NULL OR 
+	NULLIF(Source.[ObjectWhere], Target.[ObjectWhere]) IS NOT NULL OR NULLIF(Target.[ObjectWhere], Source.[ObjectWhere]) IS NOT NULL OR 
+	NULLIF(Source.[Defaults], Target.[Defaults]) IS NOT NULL OR NULLIF(Target.[Defaults], Source.[Defaults]) IS NOT NULL OR 
+	NULLIF(Source.[SQLSentence], Target.[SQLSentence]) IS NOT NULL OR NULLIF(Target.[SQLSentence], Source.[SQLSentence]) IS NOT NULL OR 
+	NULLIF(Source.[SQLConStringId], Target.[SQLConStringId]) IS NOT NULL OR NULLIF(Target.[SQLConStringId], Source.[SQLConStringId]) IS NOT NULL OR 
+	NULLIF(Source.[WebComponent], Target.[WebComponent]) IS NOT NULL OR NULLIF(Target.[WebComponent], Source.[WebComponent]) IS NOT NULL OR 
+	NULLIF(Source.[TableName], Target.[TableName]) IS NOT NULL OR NULLIF(Target.[TableName], Source.[TableName]) IS NOT NULL OR 
+	NULLIF(Source.[BadgeClass], Target.[BadgeClass]) IS NOT NULL OR NULLIF(Target.[BadgeClass], Source.[BadgeClass]) IS NOT NULL OR 
+	NULLIF(Source.[BadgeSQL], Target.[BadgeSQL]) IS NOT NULL OR NULLIF(Target.[BadgeSQL], Source.[BadgeSQL]) IS NOT NULL OR 
+	NULLIF(Source.[BadgeConStringId], Target.[BadgeConStringId]) IS NOT NULL OR NULLIF(Target.[BadgeConStringId], Source.[BadgeConStringId]) IS NOT NULL OR 
+	NULLIF(Source.[BadgeRefresh], Target.[BadgeRefresh]) IS NOT NULL OR NULLIF(Target.[BadgeRefresh], Source.[BadgeRefresh]) IS NOT NULL OR 
+	NULLIF(Source.[Enabled], Target.[Enabled]) IS NOT NULL OR NULLIF(Target.[Enabled], Source.[Enabled]) IS NOT NULL OR 
+	NULLIF(Source.[cssClass], Target.[cssClass]) IS NOT NULL OR NULLIF(Target.[cssClass], Source.[cssClass]) IS NOT NULL OR 
+	NULLIF(Source.[OriginId], Target.[OriginId]) IS NOT NULL OR NULLIF(Target.[OriginId], Source.[OriginId]) IS NOT NULL) THEN
+ UPDATE SET
+  [ParentNodeId] = Source.[ParentNodeId], 
+  [Order] = Source.[Order], 
+  [Title] = Source.[Title], 
+  [IconName] = Source.[IconName], 
+  [Descrip] = Source.[Descrip], 
+  [TypeId] = Source.[TypeId], 
+  [Params] = Source.[Params], 
+  [Url] = Source.[Url], 
+  [TargetId] = Source.[TargetId], 
+  [ProcessName] = Source.[ProcessName], 
+  [PageTypeId] = Source.[PageTypeId], 
+  [PageName] = Source.[PageName], 
+  [ReportName] = Source.[ReportName], 
+  [HelpId] = Source.[HelpId], 
+  [ReportWhere] = Source.[ReportWhere], 
+  [ObjectName] = Source.[ObjectName], 
+  [ObjectWhere] = Source.[ObjectWhere], 
+  [Defaults] = Source.[Defaults], 
+  [SQLSentence] = Source.[SQLSentence], 
+  [SQLConStringId] = Source.[SQLConStringId], 
+  [WebComponent] = Source.[WebComponent], 
+  [TableName] = Source.[TableName], 
+  [BadgeClass] = Source.[BadgeClass], 
+  [BadgeSQL] = Source.[BadgeSQL], 
+  [BadgeConStringId] = Source.[BadgeConStringId], 
+  [BadgeRefresh] = Source.[BadgeRefresh], 
+  [Enabled] = Source.[Enabled], 
+  [cssClass] = Source.[cssClass], 
+  [OriginId] = Source.[OriginId]
+WHEN NOT MATCHED BY TARGET THEN
+ INSERT([NodeId],[ParentNodeId],[Order],[Title],[IconName],[Descrip],[TypeId],[Params],[Url],[TargetId],[ProcessName],[PageTypeId],[PageName],[ReportName],[HelpId],[ReportWhere],[ObjectName],[ObjectWhere],[Defaults],[SQLSentence],[SQLConStringId],[WebComponent],[TableName],[BadgeClass],[BadgeSQL],[BadgeConStringId],[BadgeRefresh],[Enabled],[cssClass],[OriginId])
+ VALUES(Source.[NodeId],Source.[ParentNodeId],Source.[Order],Source.[Title],Source.[IconName],Source.[Descrip],Source.[TypeId],Source.[Params],Source.[Url],Source.[TargetId],Source.[ProcessName],Source.[PageTypeId],Source.[PageName],Source.[ReportName],Source.[HelpId],Source.[ReportWhere],Source.[ObjectName],Source.[ObjectWhere],Source.[Defaults],Source.[SQLSentence],Source.[SQLConStringId],Source.[WebComponent],Source.[TableName],Source.[BadgeClass],Source.[BadgeSQL],Source.[BadgeConStringId],Source.[BadgeRefresh],Source.[Enabled],Source.[cssClass],Source.[OriginId])
+WHEN NOT MATCHED BY SOURCE AND TARGET.OriginId = 1 THEN 
+ DELETE
+;
+END TRY
+BEGIN CATCH
+    DECLARE @ERRORNUMBER	INT,@ERRORMSG		VARCHAR(MAX),@ERRORSTATE		INT
+    SELECT @ERRORNUMBER = 50000 + ERROR_NUMBER(),@ERRORMSG = ERROR_MESSAGE(), @ERRORSTATE = ERROR_STATE();
+    THROW @ERRORNUMBER, @ERRORMSG, @ERRORSTATE
+END CATCH
+GO
+
+
+
+
+
--- ZulemaBD/scripts/data/pages.sql
+++ ZulemaBD/scripts/data/pages.sql
@@ -1,1 +1,68 @@
 ﻿
+
+BEGIN TRY
+
+MERGE INTO [Pages] AS Target
+USING (VALUES
+  (N'z_pg_calendario_paciente',N'generic',NULL,NULL,N'default',N'Calendario',N'vcalendar-1',N'Calendario paciente',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+ ,(N'z_pg_chat',N'generic',N'z_paciente',NULL,N'default',N'Chat',N'chat-online-1',N'Chat',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+ ,(N'z_pg_especialistas',N'list',NULL,NULL,N'default',N'Especialistas',N'nurse1',N'Especialistas',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+ ,(N'z_pg_informes_paciente',N'list',NULL,NULL,N'default',N'Informes',N'file-text-o',N'Informes paciente',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+ ,(N'z_pg_progreso_paciente',N'list',NULL,NULL,N'default',N'Progreso',N'combine-chart',N'Progreso paciente',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+ ,(N'z_pg_tareas_paciente',N'list',N'z_tareas',NULL,N'default',N'Tareas',N'deliverynotes-2',N'Tareas Paciente',NULL,NULL,0,NULL,0,0,0,NULL,0,1)
+) AS Source ([PageName],[TypeId],[ObjectName],[InterfaceName],[LayoutName],[Name],[IconName],[Descrip],[UrlRewrite],[Script],[ScriptActive],[Style],[RefreshInterval],[Sytem],[Generic],[BodyCssClass],[Offline],[OriginId])
+ON (Target.[PageName] = Source.[PageName])
+WHEN MATCHED AND (
+	NULLIF(Source.[TypeId], Target.[TypeId]) IS NOT NULL OR NULLIF(Target.[TypeId], Source.[TypeId]) IS NOT NULL OR 
+	NULLIF(Source.[ObjectName], Target.[ObjectName]) IS NOT NULL OR NULLIF(Target.[ObjectName], Source.[ObjectName]) IS NOT NULL OR 
+	NULLIF(Source.[InterfaceName], Target.[InterfaceName]) IS NOT NULL OR NULLIF(Target.[InterfaceName], Source.[InterfaceName]) IS NOT NULL OR 
+	NULLIF(Source.[LayoutName], Target.[LayoutName]) IS NOT NULL OR NULLIF(Target.[LayoutName], Source.[LayoutName]) IS NOT NULL OR 
+	NULLIF(Source.[Name], Target.[Name]) IS NOT NULL OR NULLIF(Target.[Name], Source.[Name]) IS NOT NULL OR 
+	NULLIF(Source.[IconName], Target.[IconName]) IS NOT NULL OR NULLIF(Target.[IconName], Source.[IconName]) IS NOT NULL OR 
+	NULLIF(Source.[Descrip], Target.[Descrip]) IS NOT NULL OR NULLIF(Target.[Descrip], Source.[Descrip]) IS NOT NULL OR 
+	NULLIF(Source.[UrlRewrite], Target.[UrlRewrite]) IS NOT NULL OR NULLIF(Target.[UrlRewrite], Source.[UrlRewrite]) IS NOT NULL OR 
+	NULLIF(Source.[Script], Target.[Script]) IS NOT NULL OR NULLIF(Target.[Script], Source.[Script]) IS NOT NULL OR 
+	NULLIF(Source.[ScriptActive], Target.[ScriptActive]) IS NOT NULL OR NULLIF(Target.[ScriptActive], Source.[ScriptActive]) IS NOT NULL OR 
+	NULLIF(Source.[Style], Target.[Style]) IS NOT NULL OR NULLIF(Target.[Style], Source.[Style]) IS NOT NULL OR 
+	NULLIF(Source.[RefreshInterval], Target.[RefreshInterval]) IS NOT NULL OR NULLIF(Target.[RefreshInterval], Source.[RefreshInterval]) IS NOT NULL OR 
+	NULLIF(Source.[Sytem], Target.[Sytem]) IS NOT NULL OR NULLIF(Target.[Sytem], Source.[Sytem]) IS NOT NULL OR 
+	NULLIF(Source.[Generic], Target.[Generic]) IS NOT NULL OR NULLIF(Target.[Generic], Source.[Generic]) IS NOT NULL OR 
+	NULLIF(Source.[BodyCssClass], Target.[BodyCssClass]) IS NOT NULL OR NULLIF(Target.[BodyCssClass], Source.[BodyCssClass]) IS NOT NULL OR 
+	NULLIF(Source.[Offline], Target.[Offline]) IS NOT NULL OR NULLIF(Target.[Offline], Source.[Offline]) IS NOT NULL OR 
+	NULLIF(Source.[OriginId], Target.[OriginId]) IS NOT NULL OR NULLIF(Target.[OriginId], Source.[OriginId]) IS NOT NULL) THEN
+ UPDATE SET
+  [TypeId] = Source.[TypeId], 
+  [ObjectName] = Source.[ObjectName], 
+  [InterfaceName] = Source.[InterfaceName], 
+  [LayoutName] = Source.[LayoutName], 
+  [Name] = Source.[Name], 
+  [IconName] = Source.[IconName], 
+  [Descrip] = Source.[Descrip], 
+  [UrlRewrite] = Source.[UrlRewrite], 
+  [Script] = Source.[Script], 
+  [ScriptActive] = Source.[ScriptActive], 
+  [Style] = Source.[Style], 
+  [RefreshInterval] = Source.[RefreshInterval], 
+  [Sytem] = Source.[Sytem], 
+  [Generic] = Source.[Generic], 
+  [BodyCssClass] = Source.[BodyCssClass], 
+  [Offline] = Source.[Offline], 
+  [OriginId] = Source.[OriginId]
+WHEN NOT MATCHED BY TARGET THEN
+ INSERT([PageName],[TypeId],[ObjectName],[InterfaceName],[LayoutName],[Name],[IconName],[Descrip],[UrlRewrite],[Script],[ScriptActive],[Style],[RefreshInterval],[Sytem],[Generic],[BodyCssClass],[Offline],[OriginId])
+ VALUES(Source.[PageName],Source.[TypeId],Source.[ObjectName],Source.[InterfaceName],Source.[LayoutName],Source.[Name],Source.[IconName],Source.[Descrip],Source.[UrlRewrite],Source.[Script],Source.[ScriptActive],Source.[Style],Source.[RefreshInterval],Source.[Sytem],Source.[Generic],Source.[BodyCssClass],Source.[Offline],Source.[OriginId])
+WHEN NOT MATCHED BY SOURCE AND TARGET.OriginId = 1 THEN 
+ DELETE
+;
+END TRY
+BEGIN CATCH
+    DECLARE @ERRORNUMBER	INT,@ERRORMSG		VARCHAR(MAX),@ERRORSTATE		INT
+    SELECT @ERRORNUMBER = 50000 + ERROR_NUMBER(),@ERRORMSG = ERROR_MESSAGE(), @ERRORSTATE = ERROR_STATE();
+    THROW @ERRORNUMBER, @ERRORMSG, @ERRORSTATE
+END CATCH
+GO
+
+
+
+
+
